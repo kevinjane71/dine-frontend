652d8daa323d7be982574d9a2ca77240cc7127b9
Developer <developer@kirana.com>
2025-09-23 17:10:20 +0530

    ds

--- next.config.js
+++ next.config.js
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost', 'firebasestorage.googleapis.com'],
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001',
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
